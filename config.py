c521734fb407d6ec4d35e066ad33f1395709dfcf
zypioo <tandirli.efe@gmail.com>
2025-07-09 02:31:55 +0300

    Wrote the baseline Config variables and data for an initial beggining

--- config.py
+++ config.py
@@ -1,1 +1,19 @@
+G = 6.67430e-11
 
+DT = 1           # Time step in seconds
+TOTAL_TIME = 10000  # Total simulation time in seconds
+
+
+# Initial conditions 
+INITIAL_CONDITIONS = [
+    {
+        "mass": 5.972e24,  # Earth
+        "position": [0.0, 0.0],
+        "velocity": [0.0, 0.0],
+    },
+    {
+        "mass": 7.348e22,  # Moon
+        "position": [384400000.0, 0.0],  # meters
+        "velocity": [0.0, 1022.0],       # m/s
+    },
+]
